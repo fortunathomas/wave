9d1081c96442e65d46c2306b1523bc84280b8509
Thomas <14529918@itisrossi.vi.it>
2026-01-09 19:47:00 +0100

    password

--- app/musica/page.tsx
+++ app/musica/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function MusicaPage() {
     return (
         <div>
-            <p style={{color: '#94a3b8', marginBottom: '2rem'}}>fuori presto lov u &lt;3</p>
+            <p style={{color: '#94a3b8', marginBottom: '2rem'}}>&lt;3</p>
             <br />
             <audio
                 controls
