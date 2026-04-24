e175790163943486db6ffa77be8f3c56d8ea569f
tho <14529918@itisrossi.vi.it>
2026-04-24 17:38:20 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "node:path";
+
+const projectRoot = path.resolve(__dirname);
 
 const nextConfig: NextConfig = {
 	allowedDevOrigins: ["192.168.15.105"],
+	turbopack: {
+		root: projectRoot,
+	},
 };
 
 export default nextConfig;
